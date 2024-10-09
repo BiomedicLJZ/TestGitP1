5d9647c8f66b8289c9197469fb4631730df84dbb
BiomedicLJZ <leonardo.juarez.zucco@gmail.com>
2024-10-09 14:20:42 -0600

    On branch master  Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <string>
 
 
+
+
+
 int main()
 {
-    std::string name;
-    std::cout << "Enter your name: ";
+    
+    std::cout << "Enter your age: ";
     std::cin >> name;
     std::cout << "Hello, " << name << "!" << std::endl;
     return 0;
